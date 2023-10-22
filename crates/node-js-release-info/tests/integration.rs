2bc04c3100d059ef6674e0edbb6c8c44652d1643
busticated <itsbusticated@gmail.com>
2023-10-22 12:22:25 -0700

    [node-js-release-info] add integration test for .fetch_all()

--- crates/node-js-release-info/tests/integration.rs
+++ crates/node-js-release-info/tests/integration.rs
@@ -15,7 +15,7 @@ fn it_provides_expected_resources() {
 }
 
 #[tokio::test]
-async fn it_works() {
+async fn it_fetches_node_js_release_info_for_a_given_configuration() {
     let mut info = NodeJSRelInfo::new(VERSION);
     let result = info.macos().x64().tar_gz().fetch().await.unwrap();
     assert_eq!(
@@ -24,6 +24,21 @@ async fn it_works() {
     );
     assert_eq!(
         result.sha256,
+        "ceeba829f44e7573949f2ce2ad5def27f1d6daa55f2860bea82964851fae01bc"
+    );
+}
+
+#[tokio::test]
+async fn it_fetches_node_js_release_info_for_all_supported_configurations() {
+    let info = NodeJSRelInfo::new(VERSION);
+    let result = info.fetch_all().await.unwrap();
+    assert_eq!(result.len(), 24);
+    assert_eq!(
+        result[4].url,
+        "https://nodejs.org/download/release/v20.7.0/node-v20.7.0-darwin-x64.tar.gz"
+    );
+    assert_eq!(
+        result[4].sha256,
         "ceeba829f44e7573949f2ce2ad5def27f1d6daa55f2860bea82964851fae01bc"
     );
 }
